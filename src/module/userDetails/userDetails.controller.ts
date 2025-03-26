e9a4fdaf59823527603de391080c8070b856a21d
saranya6676 <saranyachandrabose27@gmail.com>
2025-03-26 18:08:06 +0530

    Add getUser endpoint to userDetails module for retrieving user list

--- src/module/userDetails/userDetails.controller.ts
+++ src/module/userDetails/userDetails.controller.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { newUser } from "./userDetails.service";
+import { getUser } from "./userDetails.service";
 // import { auth } from "../../shared/helper";
 const userDetailRouter = Router();
 
 userDetailRouter.post('/newUser',(req, res) => { newUser(req, res);
 });
+userDetailRouter.get('/getUser',(req, res) => { getUser(req, res);
+});
+
 
 
 export default userDetailRouter;
