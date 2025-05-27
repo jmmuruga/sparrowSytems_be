366e764b6b7f3d2bae367b46a00d24f8cc1f1eee
Akshayachuz <akshayamanju2@gmail.com>
2025-05-27 17:59:11 +0530

    feat: add getHomePageCategoryToDisplay and getRecentOffersToDisplay endpoints with corresponding service logic

--- src/module/recentOffersModule/recentOffers.controller.ts
+++ src/module/recentOffersModule/recentOffers.controller.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { addRecentOffersSettings, getRecentOffersDetails } from "./recentOffers.service";
+import { addRecentOffersSettings, getRecentOffersDetails, getRecentOffersToDisplay } from "./recentOffers.service";
 
 const recentOffersRouter = Router();
 
 recentOffersRouter.post('/addRecentOffersSettings', ( req, res ) => {addRecentOffersSettings( req, res )});
-recentOffersRouter.get('/getRecentOffersDetails', (req, res) => { getRecentOffersDetails(req, res)})
+recentOffersRouter.get('/getRecentOffersDetails', (req, res) => { getRecentOffersDetails(req, res)});
+recentOffersRouter.get('/getRecentOffersToDisplay', (req, res) => { getRecentOffersToDisplay(req, res)});
 
 export default recentOffersRouter;
