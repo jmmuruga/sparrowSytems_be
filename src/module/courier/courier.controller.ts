de3043535a02c0474dc0e3909eb2b113e45965f5
saranya6676 <saranyachandrabose27@gmail.com>
2025-05-29 18:07:56 +0530

    feat: enhance courier module with status update and delete functionality

--- src/module/courier/courier.controller.ts
+++ src/module/courier/courier.controller.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addCourier, getCourier } from "./courier.services";
+import { addCourier, courierStatus, deletecourierDetails, getCourier } from "./courier.services";
 
 const courierRouter = Router();
 
@@ -7,6 +7,12 @@ courierRouter.post('/addcourier',(req, res) => { addCourier(req, res);
 });
 courierRouter.get('/getcourier',(req, res) => { getCourier(req, res);
 });
+courierRouter.put('/updatestatus/:status/:id',(req, res) => {  courierStatus(req, res);
+});
+courierRouter.delete("/deletecourier/:id", (req, res) => {
+ deletecourierDetails(req, res);
+});
+
 
 
 
--- src/module/courier/courier.services.ts
+++ src/module/courier/courier.services.ts
@@ -66,3 +66,73 @@ export const getCourier = async (req: Request, res: Response) => {
   }
 };
 
+export const courierStatus = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  console.log(id,"hh")
+
+  const statusVal: boolean = req.params.status === "true";
+  const repo = appSource.getRepository(courier);
+
+  try {
+    const typeNameFromDb = await repo
+      .createQueryBuilder("courier")
+      .where("courier.courier_id = :id", {
+        id: id,
+      })
+      .getOne();
+    if (!typeNameFromDb?.courier_id) {
+      throw new HttpException("Data not Found", 400);
+    }
+    await repo
+      .createQueryBuilder()
+      .update(courier)
+      .set({ status: statusVal })
+      .where({ courier_id: id })
+      .execute();
+
+    res.status(200).send({
+      IsSuccess: `Status Updated successfully!`,
+    });
+  } catch (error) {
+    if (error instanceof ValidationException) {
+      return res.status(400).send({
+        message: error?.message,
+      });
+    }
+    res.status(500).send(error);
+  }
+};
+
+export const deletecourierDetails = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const Repo = appSource.getRepository(courier);
+
+  try {
+    const deleteUser = await Repo.createQueryBuilder("courier")
+      .where("courier.courier_id = :id", {
+        id: id,
+      })
+      .getOne();
+    if (!deleteUser?.courier_id) {
+      throw new HttpException("id not Found", 400);
+    }
+    await Repo.createQueryBuilder("courier")
+      .delete()
+      .from(courier)
+      .where("courier_id = :id", { id: id })
+      .execute();
+    res.status(200).send({
+      IsSuccess: `id  deleted successfully!`,
+    });
+  } catch (error) {
+    if (error instanceof ValidationException) {
+      return res.status(400).send({
+        message: error?.message,
+      });
+    }
+    res.status(500).send(error);
+  }
+};
+
+
+
